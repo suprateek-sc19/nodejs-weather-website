3e4bc0d579cc6cc0df3a64b4ddedf8a9c47d28c6
Suprateek Chatterjee <suprateek1912@gmail.com>
2020-05-29 16:58:16 +0530

    Remove console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
