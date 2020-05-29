0377acd8fe658aef7bb353e44a999177ac21ee56
Suprateek Chatterjee <suprateek1912@gmail.com>
2020-05-29 20:03:10 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.innerHTML = "Loading..."
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         
         if(data.error){ 
